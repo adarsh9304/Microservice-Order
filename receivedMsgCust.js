0abc9aabc1875047294cda3da39b38a45f24e289
adarsh9304 <158045876+adarsh9304@users.noreply.github.com>
2024-09-20 11:12:20 +0530

    Update receivedMsgCust.js

--- receivedMsgCust.js
+++ receivedMsgCust.js
@@ -2,7 +2,7 @@ const amqplib=require('amqplib')
 
 async function ReceivedFromCustomer(message) {
     try{
-        const rabbitMQ='amqp://rabbitmq:5672';
+        const rabbitMQ = 'amqp://13.200.246.235:5672';
         const queueName='order_queue';
        
         const connection=await amqplib.connect(rabbitMQ);
